188b77c20869e3f536d2db93593784ccfed2c48d
Brynne Zdybel <bazdybel@gmail.com>
2017-01-11 17:12:11 -0500

    all pages redirect to login page as long as views have if current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :check_user_login
-
-  def check_user_login
-  	if current_user
-  		super
-  	else
-  		render 'devise/sessions/new'
-  	end
-  end
-
 end
--- app/helpers/boats_helper.rb
+++ app/helpers/boats_helper.rb
@@ -1,18 +1,7 @@
 module BoatsHelper
-
-# available_containers = boat.max_containers
-	# def helper remove_containers
-	# 	boat.available_containers - job.containers
-	# end
-
-	# def helper available_containers
-	# 	@boat.container_max
-	# end
-
 end
 
 
 
-# if job1.containers > boat1.available_containers
-#   then you cant do it
+
  
