a2c1160bba93c9abaca8dd2ebfe487c04ba8155a
paul <pkwdigital@gmail.com>
2017-01-11 13:39:07 -0500

    Add Abbility to Add Jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,9 +6,14 @@ class JobsController < ApplicationController
   end
 
   def new
+    @job = Job.new
   end
 
   def create
+    @job = Job.create(job_params)
+    @job.save
+    flash[:notice] = "Job Successfully Created"
+    redirect_to root_path
   end
 
   def edit
@@ -19,4 +24,12 @@ class JobsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:description, :origin, :destination, :container_amount, :cost, :user_id)
+  end
+
+
 end
