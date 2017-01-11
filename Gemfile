c275c07147446e7caab445b9ec5c2ea5de2f9213
paul <pkwdigital@gmail.com>
2017-01-11 15:43:55 -0500

    Started working with AWS & Bucket

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'devise'
+gem 'paperclip'
+gem 'aws-sdk'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :jobs
-         has_many :boats
+  has_many :jobs
+  has_many :boats
+
+  has_attached_file :avatar,
+  :storage => :s3,
+  :s3_region => "us-east-2",
+  :s3_credentials => {
+    :bucket => "shipping-app-paul",
+    :access_key_id => "AKIAJ2RNJVTCB2MNJVCA",
+    :secret_access_key => "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",
+  }
+
+  validates_attachment_content_type :avatar,
+  :content_type => /\Aimage\/.*\Z/
 end
