6f62f5e435c3e5388d3d15b45b97b5aad04914ad
paul <pkwdigital@gmail.com>
2017-01-12 13:58:28 -0500

    add some styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 User.create(email: "name@name.com", password: "password")
 
 
-Boat.create(name: "boat1", location: "Philadelphia", user_id: 1, container_max: 10)
+# Boat.create(name: "boat1", location: "Philadelphia", user_id: 1, container_max: 10)
 
 Job.create(cost: 2000.00, origin: "Philadelphia", destination: "New York", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", container_amount: 8, user_id: 1)
