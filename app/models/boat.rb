0cefef4d2a2559597adfe110468ecf70d2250ef8
paul <pkwdigital@gmail.com>
2017-01-12 13:40:28 -0500

    add ability to add photo to boat

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,7 @@ class Boat < ApplicationRecord
 	belongs_to :user
 	has_and_belongs_to_many :jobs
 
+	has_attached_file :avatar
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
