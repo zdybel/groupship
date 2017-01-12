40940be04f85f1099b00a7f031b7530ec912de2f
paul <pkwdigital@gmail.com>
2017-01-12 14:11:46 -0500

    Style Add Boat Page

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,6 +2,10 @@ class Boat < ApplicationRecord
 	belongs_to :user
 	has_and_belongs_to_many :jobs
 
+
+	has_attached_file :avatar
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 	validates :name, presence: true, uniqueness: true
 	validates :location, presence: true
 	validates :container_max, presence: true
